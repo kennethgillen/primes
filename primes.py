3922a02627d9f11671c6321c90a290ce7c240f7d
Kenneth Gillen <k.h.gillen@dundee.ac.uk>
2018-12-06 13:19:32 +0000

    Adding argparse help/description.

--- primes.py
+++ primes.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python
 
+import sys
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(
+    description='An application to calculate and output a \
+    multiplication table of made of prime numbers.'
+)
+parser.add_argument(
+    "-v", "--verbose",
+    action="store_true",
+    help="include verbose output."
+)
+
+parser.add_argument(
+    "-p", "--primesonly",
+    action="store_true",
+    help="output just a list of prime numbers."
+)
+
+parser.add_argument(
+    "count",
+    nargs=1,
+    help="the (integer) number of prime numbers you wish to calculate."
+)
+
+args = parser.parse_args()
+
 print "Stub for python application."
